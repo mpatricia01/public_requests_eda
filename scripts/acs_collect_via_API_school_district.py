53c4eabcc04c89c1af16bf29e3f3313318482978
cyouh95 <25449416+cyouh95@users.noreply.github.com>
2021-06-18 00:30:03 -0700

    update script to pull district data

--- scripts/acs_collect_via_API_school_district.py
+++ scripts/acs_collect_via_API_school_district.py
@@ -5,7 +5,7 @@ import os
 
 
 '''
-program to get census tract ACS data
+program to get census district ACS data
 API site: https://www.census.gov/data/developers/data-sets/acs-5year.html
 Variables:https://api.census.gov/data/2019/acs/acs5/variables.html
 '''
@@ -16,13 +16,13 @@ def main():
     # District data
 
     col_names = ['district_name',
-
+    
                  # Population
                  'pop_total', 'pop_total_25plus',
 
                  # Median household income
                  'median_household_income', 'median_inc_2544', 'median_inc_4564',
-
+    
                  # Race and ethnicity
                  'pop_white', 'pop_black', 'pop_amerindian', 'pop_asian',
                  'pop_nativehawaii', 'pop_otherrace', 'pop_tworaces', 'pop_hispanic',
@@ -35,21 +35,21 @@ def main():
                  'pop_otherrace_m_15-17', 'pop_otherrace_m_18-19', 'pop_otherrace_f_15-17', 'pop_otherrace_f_18-19',
                  'pop_tworaces_m_15-17', 'pop_tworaces_m_18-19', 'pop_tworaces_f_15-17', 'pop_tworaces_f_18-19',
                  'pop_hispanic_m_15-17', 'pop_hispanic_m_18-19', 'pop_hispanic_f_15-17', 'pop_hispanic_f_18-19',
-
+                 
                  # Educational attainment
-                 #'pop_edu_attain_doct', 'pop_edu_attain_prof', 'pop_edu_attain_master', 'pop_edu_attain_bach',
-                 #'pop_edu_attain_assoc', 'pop_edu_somecollege_1plusyrs', 'pop_edu_somecollege_under1yr', 'pop_edu_GED', 'pop_edu_hs',
-
+                 # 'pop_edu_attain_doct', 'pop_edu_attain_prof', 'pop_edu_attain_master', 'pop_edu_attain_bach',
+                 # 'pop_edu_attain_assoc', 'pop_edu_somecollege_1plusyrs', 'pop_edu_somecollege_under1yr', 'pop_edu_GED', 'pop_edu_hs',
+                 
                  'fips_state_code', 'district']  # last by default
 
     var_names = ['NAME',
-
+    
                  # Population
                  'B01003_001E', 'B15003_001E',
 
                  # Median household income
                  'B19013_001E', 'B19049_003E', 'B19049_004E',
-
+    
                  # Race and ethnicity
                  'B03002_003E', 'B03002_004E', 'B03002_005E', 'B03002_006E',
                  'B03002_007E', 'B03002_008E', 'B03002_009E', 'B03002_012E',
@@ -62,13 +62,13 @@ def main():
                  'B01001F_006E', 'B01001F_007E', 'B01001F_021E', 'B01001F_022E',
                  'B01001G_006E', 'B01001G_007E', 'B01001G_021E', 'B01001G_022E',
                  'B01001I_006E', 'B01001I_007E', 'B01001I_021E', 'B01001I_022E',
-
+                 
                  # Educational attainment
-                 #'B15003_025E', 'B15003_024E', 'B15003_023E', 'B15003_022E',
-                 #'B15003_021E', 'B15003_020E', 'B15003_019E', 'B15003_018E', 'B15003_017E',
+                 # 'B15003_025E', 'B15003_024E', 'B15003_023E', 'B15003_022E',
+                 # 'B15003_021E', 'B15003_020E', 'B15003_019E', 'B15003_018E', 'B15003_017E',
                  ]
 
-    save_data('school district (unified)', col_names, ','.join(var_names), 'district_raw.csv')
+    save_data(col_names, ','.join(var_names), 'district_raw.csv')
 
 
 def get_data(region, headings, cols):
@@ -77,7 +77,8 @@ def get_data(region, headings, cols):
 
     params = {
         'key': 'ab3870e2d20fc643fefe2e23361c8c2d40fbd0e1',
-        'for': '{}:*'.format(region),
+        'for': 'school district (unified):*',
+        'in': 'state:{}'.format(region),
         'get': cols
     }
 
@@ -89,15 +90,19 @@ def get_data(region, headings, cols):
     return data
 
 
-def save_data(region, headings, cols, file):
+def save_data(headings, cols, file):
 
-    data = get_data(region, headings, cols)
+    with open('./data/state.txt') as datafile:
+        rows = csv.DictReader(datafile, delimiter='|')
+        states = [row['STATE'] for row in rows if int(row['STATE']) < 60]
 
-    os.chdir("../data")
-
-    with open(file, 'w', newline='') as csvfile:
+    with open('./data/' + file, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerows(data)
+
+        for state in states:
+            print(state)
+            data = get_data(state, headings, cols)
+            writer.writerows(data)
 
 
 if __name__ == '__main__':
